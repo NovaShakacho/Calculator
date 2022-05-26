8e81d5a992087ff0e7724dfdcaf9a4168a0499e4
NovaShakacho <zhou.voyager@gmail.com>
2022-05-26 14:15:39 +0800

    debug

--- Calculator.h
+++ Calculator.h
@@ -6,18 +6,17 @@
 #include<queue>
 #include<string>
 #include<map>
-using std::cout;
-using std::endl;
-using std::map;
-using std::string;
-using std::queue;
-using std::stack;
-using std::ostream;
-using std::istream;
-enum negative_sign_case { not_negative_sign, negative_and_brackets, is_negative_number };
+enum character_case 
+{
+	is_digit,
+	is_operator,
+	is_negative_sign_bracket,
+	is_negative_sign_number
+};
+
 struct data_type
 {
-	friend ostream& operator<<(ostream& os, data_type dt);
+	//friend std::ostream& operator<<(std::ostream& os, data_type dt);
 	data_type(double _number):
 		is_symbol(false), number(_number), symbol(0) { }
 	data_type(char _char) :
@@ -27,27 +26,28 @@ struct data_type
 	char symbol;
 };
 
-inline ostream& operator<<(ostream& os, data_type dt)
-{
-	if (dt.is_symbol)
-		os << dt.symbol;
-	else
-		os << dt.number;
-	return os;
-}
+//inline ostream& operator<<(ostream& os, data_type dt)
+//{
+//	if (dt.is_symbol)
+//		os << dt.symbol;
+//	else
+//		os << dt.number;
+//	return os;
+//}
+
 class Calculator
 {
 public:
 	/* public interface */
-	Calculator(string _infix);
+	Calculator(std::string _infix);
 	double get_result();
 
 	/* temporary debug function */
-	void get_infix()
+	/*void get_infix()
 	{
 		while (!infix.empty())
 		{
-			cout << infix.front();
+			std::cout << infix.front()<<" ";
 			infix.pop();
 		}
 	}
@@ -55,105 +55,214 @@ public:
 	{
 		while (!postfix.empty())
 		{
-			cout << postfix.front();
+			std::cout << postfix.front()<<" ";
 			postfix.pop();
 		}
-	}
+	}*/
 private:
 	/* core data member */
-	string string_infix;
-	queue<data_type> infix;
-	queue<data_type> postfix;
+	std::string string_infix;
+	std::queue<data_type> infix;
+	std::queue<data_type> postfix; 
 	double result;
 
 	/* core member function */
-	queue<data_type>& simplify_infix();
-	queue<data_type>& infix_to_postfix();
+	std::queue<data_type> simplify_infix();
+	std::queue<data_type> infix_to_postfix();
 	double postfix_parsing();
 
 	/* infix_to_postfix() helper member */
 	bool is_higher_priority(char current, char stack_top);
-	map<char, int> priority;
-	map<char, int>& priority_initialize();
-	negative_sign_case is_negative_sign(int index);
+	character_case get_character_case (int index);
+	double get_digit(int& i);
 	/* postfix_parsing() helper member */
+
 };
 
-inline Calculator::Calculator(string _infix):
-	string_infix(_infix),infix(simplify_infix()),postfix(infix_to_postfix()),result(postfix_parsing()), priority(priority_initialize()) { }
+inline Calculator::Calculator(std::string _infix):
+	string_infix(_infix),infix(simplify_infix()),postfix(infix_to_postfix()),result(postfix_parsing()){ }
 
 inline double Calculator::get_result()
 {
 	return result;
 }
 
-inline queue<data_type>& Calculator::simplify_infix()
+inline std::queue<data_type> Calculator::simplify_infix()
 {
-	bool is_negative_number = false;
-	queue<data_type> infix;
-	infix.push(data_type('#'));
+	
+	std::queue<data_type> infix;
 	infix.push(data_type('('));
+	bool negative_number = false;
 	for (int i = 0; i < string_infix.size(); ++i)
 	{
-		negative_sign_case nsc = is_negative_sign(i);
+		character_case nsc = get_character_case (i);
+		switch (nsc)
+		{
+		case is_digit:
+			infix.push(data_type(get_digit(i)));
+			if (negative_number)
+			{
+				infix.push(data_type(')'));
+				negative_number = false;
+			}
+			break;
+		case is_operator:
+			infix.push(data_type(string_infix[i]));
+			break;
+		case is_negative_sign_bracket:
+			infix.push(data_type(0.0));
+			infix.push(data_type('-'));
+			break;
+		case is_negative_sign_number:
+			negative_number = true;
+			infix.push(data_type('('));
+			infix.push(data_type(0.0));
+			infix.push(data_type('-'));
+			break;
+		default:
+			std::cout << "Error in simplify_infix!";
+			break;
+		}
 	}
 	infix.push(data_type(')'));
 	return infix;
 }
 
-inline queue<data_type>& Calculator::infix_to_postfix()
+inline std::queue<data_type> Calculator::infix_to_postfix()
 {
-	queue<data_type> postfix;
-	stack<char> operators;
-	operators.push('#');
-	/*for (int i = 0; i < string_infix.size(); ++i)
+	
+	//The logic is in the document.
+	std::queue<data_type> postfix;
+	std::stack<char> operators;
+	char ops;
+	while (!infix.empty())
 	{
-		switch (string_infix[i])
+		//digit
+		if (!infix.front().is_symbol)
+			postfix.push(infix.front());
+		//operator 
+		else
 		{
-		case '-':
-		case '+':
-		case '*':
-		case '/':
-		case '(':
-		case ')':
-		default:
-			break;
+			//The highest priority element must be at the top of the std::stack.
+			ops = infix.front().symbol;
+			switch (ops)
+			{
+			case '+':
+			case '-':
+			case '*':
+			case '/':
+				if (is_higher_priority(ops, operators.top()))
+					operators.push(ops);
+				else
+				{
+					while (!is_higher_priority(ops, operators.top()))
+					{
+						postfix.push(data_type(operators.top()));
+						operators.pop();
+					}
+					operators.push(ops);
+				}
+				break;
+			case '(':
+				operators.push(ops);
+				break;
+			case ')':
+				while (operators.top() != '(')
+				{
+					postfix.push(data_type(operators.top()));
+					operators.pop();
+				}
+				//pop '('
+				operators.pop();
+				break;
+			default:
+				std::cout << "Error in infix_to_postfix!";
+				break;
+			}
 		}
-	}*/
+		infix.pop();
+	}
+	//Process the remaining elements on the std::stack.
+	while (!operators.empty())
+	{
+		postfix.push(operators.top());
+		operators.pop();
+	}
+	return postfix;
+}
+
+inline double Calculator::postfix_parsing()
+{
+	std::stack<double> digit;
+	while (!postfix.empty())
+	{
+		if (!postfix.front().is_symbol)
+			digit.push(postfix.front().number);
+		else
+		{
+			double second = digit.top();
+			digit.pop();
+			double first = digit.top();
+			digit.pop();
+			switch (postfix.front().symbol)
+			{
+			case '+':
+				digit.push(first + second);
+				break;
+			case '-':
+				digit.push(first - second);
+				break;
+			case '/':
+				digit.push(first / second);
+				break;
+			case '*':
+				digit.push(first * second);
+				break;
+			default:
+				std::cout << "Error is in postfix_parsing";
+				break;
+			}
+		}
+		postfix.pop();
+	}
+	return digit.top();
 }
 
 inline bool Calculator::is_higher_priority(char current,char stack_top)
 {
-	return priority[current]> priority[stack_top];
-}
-
-inline map<char, int>& Calculator::priority_initialize()
-{
-	map<char, int> priority;
+	std::map<char, int> priority;
 	priority['#'] = 0;
 	priority['('] = 1;
 	priority['+'] = 2;
 	priority['-'] = 2;
 	priority['*'] = 3;
 	priority['/'] = 3;
-	return priority;
+	return priority[current] > priority[stack_top];
 }
 
-inline negative_sign_case  Calculator::is_negative_sign(int index)
+inline character_case Calculator::get_character_case (int index)
 {
-	if (!(string_infix[index] == '-' && (index == 0 || string_infix[index - 1] == '(')))
-		return not_negative_sign;
-	if (string_infix[index + 1]==')')
-		return negative_and_brackets;
-	return is_negative_number;
+	if (isdigit(string_infix[index]))
+		return is_digit;
+	else if (string_infix[index] == '-' && (index == 0 || string_infix[index - 1] == '('))
+	{
+		if (isdigit(string_infix[index + 1]))
+			return is_negative_sign_number;
+		else
+			return is_negative_sign_bracket;
+	}
+	else
+		return is_operator;
 }
 
-
-
-
-
-
-
-
-
-
+inline double Calculator::get_digit(int& i)
+{
+	std::string str_digit;
+	while (isdigit(string_infix[i+1])|| string_infix[i+1]=='.')
+	{
+		str_digit += string_infix[i];
+		++i;
+	}
+	str_digit += string_infix[i];
+	return std::stod(str_digit);
+}
