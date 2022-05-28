f1d3ead357b8c42c14617c7d08992897e754ab06
NovaShakacho <zhou.voyager@gmail.com>
2022-05-28 11:45:32 +0800

    Add files close

--- Source.cpp
+++ Source.cpp
@@ -19,7 +19,7 @@ int main(void)
 	//Can't use ios::binary!
 	//The default(windows) newline character of txt file is CRLF(\r\n), but getline is \n.
 	//Non-binary mode \r\n will be automatically converted to \n by the system, while binary mode will not.
-	ifstream ifs("input1.txt",ios::in);
+	ifstream ifs("input.txt",ios::in);
 	ofstream ofs("output.txt",ios::out);
 	while (getline(ifs, infix))
 	{
